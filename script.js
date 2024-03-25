46726c04eb709294f280c439208b25f307d9b6ce
Karina Yang <yangchenkarina@gmail.com>
2024-03-25 13:28:07 -0300

    Adicionado um título e um botão que qunado passamos o mouse por cima, o background do bady muda de cor

---
+++ script.js
@@ -0,0 +1,12 @@
+let botao = document.getElementById("botao");
+botao.addEventListener('mouseover', clicar)
+botao.addEventListener('mouseout', sair)
+let corpo = document.getElementById("corpo");
+
+function clicar() {
+    corpo.style.backgroundColor = 'yellow';
+};
+
+function sair() {
+    corpo.style.backgroundColor = 'aquamarine';
+}
